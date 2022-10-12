2d459817f5d9e9a3cd77e49e1981f8175e8b4ab0
hyunganom <dlguswo0504@naver.com>
2022-10-12 15:20:07 +0900

    DB테스트, application 설정

---
+++ semiproject/src/test/java/com/kh/semi/Test.java
@@ -0,0 +1,19 @@
+package com.kh.semi;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.jdbc.core.JdbcTemplate;
+
+@SpringBootTest
+public class Test {
+	
+	@Autowired
+	JdbcTemplate jdbcTemplate;
+	 
+	@org.junit.jupiter.api.Test
+	public void test() {
+		String sql = "insert into test1(no, name) values(1,'가나다')";
+		jdbcTemplate.update(sql);
+	}
+
+}
