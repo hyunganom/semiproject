890ae8206a597e7b012cd652ab2ad1a18cd62bd9
JENN-YOO <109942726+JENN-YOO@users.noreply.github.com>
2022-10-12 18:34:20 +0900

    Add files via upload
    테이블 수정본 재업로드

---
+++ DB table/쿠폰(coupon).sql
@@ -0,0 +1,23 @@
+-- 쿠폰 테이블
+-- 발행될 쿠폰에 대한 정보
+
+-- 구성요소
+-- 쿠폰 번호(coupon_no) 중복되면 안됨, 기본키, 시퀀스 부여
+-- 쿠폰 이름(coupon_name) 한글 10자 이내
+-- 쿠폰 발행일(coupon_startdate) 쿠폰이 발행 날짜, 지정 설정
+-- 쿠폰 만료일(coupon_enddate) 쿠폰 만료 날짜, 지정 설정
+-- 쿠폰 할인율(coupon_discount) 계산식이 들어갈거니 숫자로 설정
+-- 쿠폰 정보(coupon_info) 쿠폰이 발행된 이벤트 명(생일축하, 신규가입축하 등)
+
+create table coupon (
+coupon_no number primary key,
+coupon_name varchar2(30) not null,
+coupon_startdate date default sysdate,
+coupon_enddate date not null,
+coupon_discount number not null,
+coupon_info varchar2(60) not null
+);
+
+drop table coupon;
+
+create sequence coupon_seq;
---
+++ DB table/쿠폰발행현황(coupon_list).sql
@@ -0,0 +1,13 @@
+-- 쿠폰리스트 테이블
+-- 쿠폰발행 현황 정보
+
+-- 구성요소
+-- 쿠폰 번호(coupon_list_no) 쿠폰테이블의 coupon_no참조
+-- 보유자 아이디(coupon_list_member_id)  회원테이블의 member_id  참조
+
+create table coupon_list (
+coupon_list_no number references coupon(coupon_no) on delete cascade,
+coupon_list_member_id varchar2(20) references member(member_id) on delete cascade 
+);
+
+drop table coupon_list
