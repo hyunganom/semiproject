7887e8fe8f18340844edde7784ca884da1616d7f
Lee-seungjung <109942536+Lee-seungjung@users.noreply.github.com>
2022-10-12 17:25:22 +0900

    Delete 장바구니(basket).sql

--- DB table/장바구니(basket).sql
+++
@@ -1,8 +0,0 @@
--- BASKET 테이블 생성
-create table basket (
-basket_id varchar2(20) references member(member_id) on delete cascade,
-basket_product_no number references product(product_no) on delete cascade,
-basket_count_number default 1 check(basket_count_number >= 1),
-basket_adddate date default sysdate,
-primary key(basket_id, basket_product_no)
-);
