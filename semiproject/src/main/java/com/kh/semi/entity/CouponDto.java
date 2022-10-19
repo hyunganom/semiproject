212cbce4725cd9442bb3f2814c58112306e47ca4
JENN-YOO <hyali6629@naver.com>
2022-10-19 11:47:59 +0900

    롬복 추가

--- semiproject/src/main/java/com/kh/semi/entity/CouponDto.java
+++ semiproject/src/main/java/com/kh/semi/entity/CouponDto.java
@@ -1,7 +1,12 @@
 package com.kh.semi.entity;
 
 import java.sql.Date;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
+@Data @NoArgsConstructor @AllArgsConstructor @Builder
 public class CouponDto {
 	private int couponNo;
 	private String couponId;
--- semiproject/src/main/java/com/kh/semi/entity/CouponListDto.java
+++ semiproject/src/main/java/com/kh/semi/entity/CouponListDto.java
@@ -1,5 +1,12 @@
 package com.kh.semi.entity;
 
+import java.sql.Date;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data @NoArgsConstructor @AllArgsConstructor @Builder
 public class CouponListDto {
 	private int couponListNo;
 	private String couponName;
