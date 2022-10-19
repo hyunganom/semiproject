70d86d8c44af6016fa57ba7287ae144b43654113
JeongHwaP <pjm2327@naver.com>
2022-10-19 18:14:54 +0900

    회원목록 Controller, Dao, DaoImpl / 로그인, 메인페이지 구현중

--- semiproject/src/main/java/com/kh/semi/repository/MemberDaoImpl.java
+++ semiproject/src/main/java/com/kh/semi/repository/MemberDaoImpl.java
@@ -1,7 +1,12 @@
 package com.kh.semi.repository;
 
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
 import org.springframework.stereotype.Repository;
 
 import com.kh.semi.entity.MemberDto;
@@ -27,6 +32,44 @@ public class MemberDaoImpl implements MemberDao{
 				memberDto.getMemberBirth(), memberDto.getMemberGender()
 		};
 		jdbcTemplate.update(sql, param);
+	}
+	
+	private RowMapper<MemberDto> mapper = new RowMapper<MemberDto>() {
+		@Override
+		public MemberDto mapRow(ResultSet rs, int rowNum) throws SQLException {
+			MemberDto dto = new MemberDto();
+			dto.setMemberId(rs.getString("member_id"));
+			dto.setMemberPw(rs.getString("member_pw"));
+			dto.setMemberName(rs.getString("member_name"));
+			dto.setMemberEmail(rs.getString("member_email"));
+			dto.setMemberTel(rs.getString("member_tel"));
+			dto.setMemberPost(rs.getString("member_post"));
+			dto.setMemberBaseAddress(rs.getString("member_base_address"));
+			dto.setMemberDetailAddress(rs.getString("member_detail_address"));
+			dto.setMemberBirth(rs.getDate("member_birth"));
+			dto.setMemberGender(rs.getString("member_gender"));
+			dto.setMemberGrade(rs.getString("member_grade"));
+			dto.setMemberPoint(rs.getInt("member_point"));
+			dto.setMemberJoindate(rs.getDate("member_joindate"));
+			dto.setMemberLogindate(rs.getDate("member_logindate"));
+			dto.setMemberWithdrawal(rs.getString("member_withdrawal"));
+			dto.setMemberWithdrawalDate(rs.getDate("member_withdrawaldate"));
+			return dto;
+		}
+	};
+
+	@Override
+	public List<MemberDto> selectList() {
+		String sql = "select * from member";
+		return jdbcTemplate.query(sql, mapper);
+	}
+
+	@Override
+	public List<MemberDto> selectList(String type, String keyword) {
+		String sql = "select * from member where instr(#1, ?) > 0 order by #1 asc";
+		sql = sql.replace("#1", type);
+		Object[] param = {keyword};
+		return jdbcTemplate.query(sql, mapper, param);
 	}
 	
 }
---
+++ semiproject/src/main/webapp/WEB-INF/views/member/login.jsp
@@ -0,0 +1,23 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>회원 로그인</title>
+</head>
+<body>
+	<div align="cemter">
+		<h1>회원 로그인</h1>
+		<form action="login" method="post">
+			<input type="text" name="memberId" placeholder="아이디" required><br><br>
+			<input type="password" name="memberPw" placeholder="비밀번호" required><br><br>
+			<button type="submit">로그인</button>
+		</form>
+		
+		<c:if test="${param.error != null}">
+			<h2 style="color:red">입력한 정보가 맞지 않습니다.</h2>
+		</c:if>
+	</div>
+</body>
+</html>
