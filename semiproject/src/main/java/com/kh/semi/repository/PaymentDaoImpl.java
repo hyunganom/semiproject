f3fa98362ed23c3a5c0fc8810b960079542cf720
Lee-seungjung <tmdwjd0295@naver.com>
2022-10-20 11:39:13 +0900

    paymentDao 구현중

--- semiproject/src/main/java/com/kh/semi/repository/PaymentDaoImpl.java
+++ semiproject/src/main/java/com/kh/semi/repository/PaymentDaoImpl.java
@@ -4,6 +4,8 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.ResultSetExtractor;
+import org.springframework.jdbc.core.RowMapper;
 import org.springframework.stereotype.Repository;
 
 import com.kh.semi.entity.PaymentDto;
@@ -17,27 +19,88 @@ public class PaymentDaoImpl implements PaymentDao{
 	//1. 등록(추가)
 	@Override
 	public void insert(PaymentDto paymentDto) {
-		// TODO Auto-generated method stub
-		
+		String sql = "insert into payment("
+				+ "payment_no, payment_order_no, payment_product_no, "
+				+ "payment_count, payment_price, payment_option) values("
+				+ "payment_seq.nextval, ?, ?, ?, ?, ?)";
+		Object[] param = {
+				paymentDto.getPaymentOrderNo(),
+				paymentDto.getPaymentProductNo(),
+				paymentDto.getPaymentCount(),
+				paymentDto.getPaymentPrice(),
+				paymentDto.getPaymentOption()
+		};
+		jdbcTemplate.update(sql, param);
 	}
 
 	//2. 수정(상품번호로 조회 후 수량/가격을 변경)
 	@Override
 	public boolean update(PaymentDto paymentDto) {
-		// TODO Auto-generated method stub
-		return false;
+		String sql = "update payment set "
+				+ "payment_count=?, payment_price=? "
+				+ "where payment_product_no=?;";
+		Object[] param = {paymentDto.getPaymentCount(),
+				paymentDto.getPaymentPrice(),
+				paymentDto.getPaymentProductNo()};
+		return jdbcTemplate.update(sql, param)>0;
 	}
 
+	//mapper 생성
+	private RowMapper<PaymentDto> mapper =(rs, idx)->{
+		PaymentDto dto = new PaymentDto();
+		dto.setPaymentNo(rs.getInt("payment_no"));
+		dto.setPaymentOrderNo(rs.getInt("payment_order_no"));
+		dto.setPaymentProductNo(rs.getInt("payment_product_no"));
+		dto.setPaymentCount(rs.getInt("payment_count"));
+		dto.setPaymentPrice(rs.getInt("payment_price"));
+		dto.setPaymentOption(rs.getString("payment_option"));
+		return dto;
+	};
+	
+	
 	//3. 조회
 	@Override
 	public List<PaymentDto> selectList() {
-		// TODO Auto-generated method stub
-		return null;
+		String sql = "select * from payment";
+		return jdbcTemplate.query(sql, mapper);
+	}
+	
+	//extractor 생성
+	private ResultSetExtractor<PaymentDto> extractor = (rs) ->{
+		if(rs.next()) {
+			PaymentDto dto = new PaymentDto();
+			dto.setPaymentNo(rs.getInt("payment_no"));
+			dto.setPaymentOrderNo(rs.getInt("payment_order_no"));
+			dto.setPaymentProductNo(rs.getInt("payment_product_no"));
+			dto.setPaymentCount(rs.getInt("payment_count"));
+			dto.setPaymentPrice(rs.getInt("payment_price"));
+			dto.setPaymentOption(rs.getString("payment_option"));
+			return dto;
+		}else {
+			return null;
+		}
+	};
+	
+	//3-1. 조회(주문번호로 조회)
+	@Override
+	public PaymentDto selectOne(int orderNo) {
+		String sql="select * from payment where payment_order_no=?";
+		Object[] param= {orderNo};
+		return jdbcTemplate.query(sql, extractor, param);
 	}
 
-	//4. 삭제(상품번호로 조회 후 삭제)
+	//4. 삭제(주문번호로 조회 후 삭제)
+	// 나중에 넘어오는 값 확인 후 매개변수 수정 예정
 	@Override
-	public boolean delete(int payment_product_no) {
+	public boolean delete(int orderNo) {
+		String sql = "delete payment where payment_order_no=?";
+		return false;
+	}
+	
+	//4-1. 삭제(주문번호와 상품번호로 조회 후 삭제)
+	// 나중에 넘어오는 값 확인 후 매개변수 수정 예정 
+	@Override
+	public boolean delete(int orderNo, int productNo) {
 		// TODO Auto-generated method stub
 		return false;
 	}
