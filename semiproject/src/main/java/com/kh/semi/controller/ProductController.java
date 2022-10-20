f067060a388f04711113e7b9741e46b86c2f4612
leeseungjung <tmdwjd0295@naver.com>
2022-10-20 22:48:01 +0900

    .

--- semiproject/src/main/java/com/kh/semi/controller/ProductController.java
+++ semiproject/src/main/java/com/kh/semi/controller/ProductController.java
@@ -2,7 +2,6 @@ package com.kh.semi.controller;
 
 import java.io.File;
 import java.io.IOException;
-import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -11,6 +10,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
