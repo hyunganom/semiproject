49f9a4a8a0063c7f89159403264e58dedc802c3c
JENN-YOO <109942726+JENN-YOO@users.noreply.github.com>
2022-10-26 10:43:47 +0900

    Update 쿠폰사용내역(coupon_use).sql
    쿠폰사용내역 테이블 컬럼 수정 및 참조 수정

--- DB table/쿠폰사용내역(coupon_use).sql
+++ DB table/쿠폰사용내역(coupon_use).sql
@@ -4,20 +4,26 @@
 -- 구성요소
 -- 기록 번호(coupon_use_no) 중복되면 안됨, 기본키, 시퀀스 부여
 -- 주문 번호(coupon_use_order_no) 외래키, 주문(orders)테이블의 주문번호(order_no)참조
--- 쿠폰 번호(coupon_origin_no) 외래키, 쿠폰리스트(coupon_list)테이블의 쿠폰번호(coupon_list_no)참조
--- 아이디(coupon_use_id) 외래키, 회원(member)테이블의 아이디(member_id)참조
+-- 발급 번호(coupon_origin_no) 외래키, 쿠폰리스트(coupon_list)테이블의 발급번호(coupon_issue)참조
 -- 쿠폰 사용일(coupon_use_date) 쿠폰 이용 날짜
-
 
 
 create table coupon_use(
 coupon_use_no number primary key,
 coupon_use_order_no references orders(order_no),
-coupon_origin_no references coupon_list(coupon_list_no),
-coupon_use_id references member(member_id),
+coupon_issue_no references coupon(coupon_issue),
 coupon_use_date date default sysdate not null
 );
                         
-drop table coupon;
+drop table coupon_use;
 
 create sequence coupon_use_seq;
+
+insert into coupon_use(coupon_use_no, coupon_use_order_no, coupon_issue_no, coupon_use_date)
+values (coupon_use_seq.nextval, 100077, 2, sysdate);
+
+
+commit;
+
+select * from orders;
+select * from coupon_use;
