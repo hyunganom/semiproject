35b105a2a87c2b9e4ef4819bdb2da9ffd5847e8d
hyunganom <dlguswo0504@naver.com>
2022-10-27 23:56:20 +0900

    리뷰작성 첨부파일
    - 리뷰작성이미지등록기능 컨트롤러,DAO, JSP기능구현

--- semiproject/src/main/java/com/kh/semi/controller/ReviewController.java
+++ semiproject/src/main/java/com/kh/semi/controller/ReviewController.java
@@ -1,13 +1,21 @@
 package com.kh.semi.controller;
 
+import java.io.File;
+import java.io.IOException;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
 
+import com.kh.semi.entity.AttachmentDto;
 import com.kh.semi.entity.ReviewDto;
+import com.kh.semi.repository.AttachmentDao;
 import com.kh.semi.repository.ReviewDao;
 
 @Controller
@@ -17,6 +25,11 @@ public class ReviewController {
 	// 의존성 주입
 	@Autowired
 	private ReviewDao reviewDao;
+	
+	@Autowired
+	private AttachmentDao attachmentDao;
+	
+	private final File reviewImg = new File("D:\\saluv\\reviewImg");
 	
 	// 1. 리뷰 작성
 	// - 리뷰 작성 Mapping
@@ -29,7 +42,9 @@ public class ReviewController {
 	
 	// - DB 처리 및 강제 이동
 	@PostMapping("/write")
-	public String write(@ModelAttribute ReviewDto reviewDto) {
+	public String write(@ModelAttribute ReviewDto reviewDto,
+			@RequestParam List<MultipartFile> attachmentReviewImg//리뷰이미지 첨부파일에 관한 파라미터
+			) throws IllegalStateException, IOException {
 		
 		// 리뷰 등록을 위해 다음 시퀀스 번호 반환
 		int reviewNo = reviewDao.nextSequence();
@@ -39,6 +54,29 @@ public class ReviewController {
 		
 		// DB에 등록(INSERT) 처리
 		reviewDao.writeReview(reviewDto);
+		
+		//리뷰 첨부파일 이미지 등록처리
+		//1. 시퀀스 발급
+		for(MultipartFile file : attachmentReviewImg) {
+			if(!file.isEmpty()) {
+			//첨부파일 시퀀스 발급
+			int attachmentNo = attachmentDao.sequence();
+			//첨부 DB등록
+			attachmentDao.insert(AttachmentDto.builder()
+					.attachmentNo(attachmentNo)
+					.attachmentName(file.getOriginalFilename())
+					.attachmentType(file.getContentType())
+					.attachmentSize(file.getSize())
+				.build());
+			
+			//파일저장
+			File target = new File(reviewImg, String.valueOf(attachmentNo));
+			reviewImg.mkdirs();//폴더 생성 명령
+			file.transferTo(target);//해당폴더에 변환과정을 거쳐서 파일등록
+			//review_attachment 연결테이블 정보 저장
+			attachmentDao.reviewConnectAttachment(reviewNo, attachmentNo);
+			}
+		}
 		
 		return "redirect:/";
 	}
--- semiproject/src/main/webapp/WEB-INF/views/review/write.jsp
+++ semiproject/src/main/webapp/WEB-INF/views/review/write.jsp
@@ -6,12 +6,13 @@
 	<jsp:param value="리뷰 작성" name="title"/>
 </jsp:include>
 
-<form action = "write" method = "post">
+<form action = "write" method = "post" enctype = "multipart/form-data">
 
-리뷰 제목 : <input name = "reviewTitle" type = "text">
-리뷰 별점 : <input name = "reviewGood" type = "number">
-리뷰 내용 : <textarea name = "reviewContent"></textarea>
-
+리뷰 제목 : <input name = "reviewTitle" type = "text"><br>
+리뷰 별점 : <input name = "reviewGood" type = "number"><br>
+리뷰 내용 : <textarea name = "reviewContent"></textarea><br>
+리뷰 이미지 : <input name = "attachmentReviewImg" type="file" accept = ".png, .jpg" multiple><br>
+<button type = "submit">등록</button>
 </form>
 
 
